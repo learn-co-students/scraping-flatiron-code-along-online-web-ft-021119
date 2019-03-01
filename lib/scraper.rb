9c3982179281533b6006669a2acbb9b0ad3d707a
Joan Lyness <joanrigdon@gmail.com>
2019-03-01 02:05:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,41 +5,44 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  attr_accessor :doc
-
 
   def initialize
-    @doc = []
+    @@all = []
   end
 
   def get_page
     html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses')
-    @doc = Nokogiri::HTML(html)
-    #binding.pry
+    doc = Nokogiri::HTML(html)
+    @@doc = doc
   end
 
   def get_courses
-    @doc.css("section#course-grid article")
-  end
-
-    # html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses')
-    # @doc = (Nokogiri::HTML(html)).css("section#course-grid article")
+    @@doc.css("section#course-grid article")
   end
 
   def make_courses
-    @doc
-    binding.pry
+    html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses')
+    doc = Nokogiri::HTML(html)
+    courses = @@doc.css("section#course-grid article").text.split("  Learn More  ")
+    courses.each do |course|
+      if course
+        @@all << course
+        @title = course.split("Full-Time")[0].strip
+        @schedule = course.scan(/....(-Time)/)
+        @description = course.split("Full-Time")[1].strip
+      end
+    end
   end
 
-
   def print_courses
-  self.make_courses
-  Course.all.each do |course|
-    if course.title
-      puts "Title: #{course.title}"
-      puts "  Schedule: #{course.schedule}"
-      puts "  Description: #{course.description}"
-    end
+    @title
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
     end
   end
 
