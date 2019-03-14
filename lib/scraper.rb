3424c93b7563e04741ae4b873949652cebb8844b
Jacqi Entzminger <jackie.entz@gmail.com>
2019-03-14 09:00:08 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,21 +34,21 @@ class Scraper
     course.description = post.css("p").text
   end
 end 
-  #   def print_courses
-  #   self.make_courses
-  #   Course.all.each do |course|
-  #     if course.title
-  #       puts "Title: #{course.title}"
-  #       puts "  Schedule: #{course.schedule}"
-  #       puts "  Description: #{course.description}"
-  #     end
-  #   end
-  # end
+   def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
   
 end
-Scraper.new.get_page 
+#Scraper.new.get_page 
 
-#Scraper.new.print_courses
+Scraper.new.print_courses
 
 
 
